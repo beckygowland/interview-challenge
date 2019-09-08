83322d5c6378382c48c3bebcc7e83f01e130c95a
becky g <beckygowland@gmail.com>
2019-09-08 11:03:19 +0100

    Add redux store to index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-reboot.min.css';
+import reduxStore from './reduxStore';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// creating a redux store for global state 
+// while this app currently does not need global store 
+// (everything could be done in local state in App.js and callback props),
+// chances are that as this app grew, it would need global state.
+// .ie if a user navigates to a checkout screen or signin page 
+const store = createStore(reduxStore)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reduxStore.js
@@ -0,0 +1,44 @@
+// As this file got bigger, it should be split into an action 
+// and reducer file. If the reducer switch case started to get
+// too complicated/nested, it could also be broken down into
+// smaller functions. Reducers could also be split apart and 
+// later combined while creating the store (combineReducers)
+
+const FOOD_SELECTED = 'FOOD_SELECTED';
+const FOOD_DELETED = 'FOOD_DELETED';
+
+export function addFoodItem(item) {
+    return { type: FOOD_SELECTED, item }
+}
+
+export function deleteFoodItem(id, position) {
+    return { type: FOOD_DELETED, id, position }
+}
+
+//////////////////////////////////////////////////
+
+const initialState = {
+    selectedItems: [],
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case FOOD_SELECTED:
+            return {
+                selectedItems: [...state.selectedItems, {...action.item}],
+            }
+        case FOOD_DELETED:
+            // There may be multiple items with the same id.
+            // Find the index of the item with the id and order
+            const itemsWithId = state.selectedItems
+                .map(({id}, index) => ({id, index}))
+                .filter(({id}) => id === action.id)
+            const itemIndex = itemsWithId[action.position].index;
+            return {
+                selectedItems: state.selectedItems
+                    .filter((item, index) => itemIndex !== index),
+            }
+        default:
+            return state
+    }
+}
